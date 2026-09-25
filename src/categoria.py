280f715e9ffd3dcf248a1b68b507978fa5ff398c
Amanda Barros <abarros.santoss@gmail.com>
2026-09-25 05:51:41 -0300

    Enhance Categoria class with descricao attribute
    Added a description attribute and updated methods.

--- src/categoria.py
+++ src/categoria.py
@@ -1,14 +1,23 @@
 class Categoria:
-    def __init__(self, nome):
+    def __init__(self, nome, descricao):
         self._nome = nome
+        self._descricao = descricao
 
     @property
     def nome(self):
         return self._nome
 
     @nome.setter
-    def nome(self, nome):
-        self._nome = nome
+    def nome(self, novo_nome):
+        self._nome = novo_nome
 
-    def exibirCategoria(self):
+    @property
+    def descricao(self):
+        return self._descricao
+
+    @descricao.setter
+    def descricao(self, nova_descricao):
+        self._descricao = nova_descricao
+
+    def __str__(self):
         return self._nome
